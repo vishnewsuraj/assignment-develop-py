e6705d99da5a38bc1e2ec8c21c7dbff5e66dcd6f
Suraj vishwakarma <90332766+vishwakarmasuraj@users.noreply.github.com>
2022-08-07 01:29:03 +0530

    feat: In progress order and unordered dict

--- allCode.py
+++ allCode.py
@@ -1,6 +1,5 @@
-
+'''
 # factorial
-
 enter = int(input("Enter your number : "))
 factorial = 1
 
@@ -12,8 +11,9 @@ else:
     for i in range(1, enter + 1):
         factorial = factorial * i
     print("The factorial of",enter,"is",factorial)
+'''
 
-
+'''
 # Find n-th fibonacci number
 
 def fabonacci(n):
@@ -27,9 +27,9 @@ def fabonacci(n):
         return fabonacci(n-1)+fabonacci(n-2)
 
 print(fabonacci(10))
+'''
 
-
-
+'''
 # Check if a number is prime or not
 
 primeNumber = int(input("Enter your prime number : "))
@@ -45,9 +45,9 @@ if flag:
     print(primeNumber, "is not a prime number")
 else:
     print(primeNumber, "is a prime number")
+'''
 
-
-
+'''
 # Rotate a list by n places (accept list, and integer as input)
 
 number = int(input("Enter your number : "))
@@ -57,9 +57,9 @@ if number > len(listOne):
 listOne = (listOne[-number:] + listOne[:-number])
 
 print(listOne)
+'''
 
-
-
+'''
 # Find all characters that appear more than once in a string
 
 checkString = 'aaaaa'
@@ -73,22 +73,36 @@ for s in checkString:
 for key in count:
     if count[key] > 1:
         print(key , count[key])
+'''
 
-
-
+'''
 # Replace all occurrences of a word in a string (accepted as input the word u want to replace)
 
 enterString = input("Enter your string : ")
 str = "Hey guys how are you and how is your going on"
 str = str.replace(enterString, '$')
 print(str)
-
-
-'''
-Accept some text as input and print all words in descending order of their occurrence
 '''
 
+'''
+# Accept some text as input and print all words in descending order of their occurrence
 
+enterText = input("Enter your text : ")
+def descText(s):
+    s.sort(reverse= True)
+    str1 = ''.join(s)
+    print(str1)
+
+def newFunc():
+    s = list(enterText)
+
+    descText(s)
+
+if __name__=="__main__":
+    newFunc()
+'''
+
+'''
 # Check if a year is a leap year or not
 
 year = int(input("Enter your year : "))
@@ -98,7 +112,10 @@ if (year % 4 == 0) and (year % 100 != 0):
     print("{0} is leap year : ".format(year))
 else:
     print("{0} is not a leap year : ".format(year))
+'''
 
+'''
+# 1 - Create ordered dictionary from an unordered dictionary
 
 from collections import OrderedDict
 
@@ -108,7 +125,10 @@ domain = OrderedDict([('de', 'Germany'),
                       ('us', 'United States'),
                       ('no', 'Norway')])
 print('domain', domain)
+'''
 
+
+# 2 - Create ordered dictionary from an unordered dictionary
 
 
 
