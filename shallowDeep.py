3f5b38762930d29f4837ddf46969202872e05880
Suraj vishwakarma <90332766+vishwakarmasuraj@users.noreply.github.com>
2022-08-07 21:14:02 +0530

    feat: Completed Shallow copy and deep copy

---
+++ shallowDeep.py
@@ -0,0 +1,16 @@
+import copy
+# Shallow copy
+
+data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+newData = copy.copy(data)
+
+newData[0][2] = 'c'
+print('New Shallow copy data : {}'.format(newData))
+print('Previous Shallow copy data : {}'.format(data))
+
+# Deep copy
+
+newDeepCopy = copy.deepcopy(data)
+newDeepCopy[0][2] = 'c'
+print('New deep copy data : {}'.format(newDeepCopy))
+print('Previous data : {}'.format(data))
